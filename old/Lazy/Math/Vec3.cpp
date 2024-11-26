40b28c4cd8633b0f070caf84d78c86cadc514710
gusrlLee <gusrl3204@naver.com>
2024-11-26 21:09:33 +0900

    restart and then

--- old/Lazy/Math/Vec3.cpp
+++
@@ -1,8 +0,0 @@
-#include "Math/Vec3.h"
-
-namespace Lazy
-{
-    Vec3::Vec3() : e{0.0f, 0.0f, 0.0f} {}
-    Vec3::Vec3(float value) : e{value, value, value} {}
-    Vec3::Vec3(float e1, float e2, float e3) : e{e1, e2, e3} {}
-}
--- old/ray.cpp
+++
@@ -1,8 +0,0 @@
-#include "ray.h"
-
-namespace lazy 
-{
-    ray::ray() {}
-    ray::ray(const point3 &o, const vec3 &d) : m_orig(o), m_dir(d) {} 
-
-}
